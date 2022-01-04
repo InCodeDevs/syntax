8d9f5014feff7e9b10f2d4be86fd49c714d4d1aa
MCTzOCK <dev@mctzock.de>
2022-01-04 21:02:11 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,3 +2,44 @@
  * @author Ben Siebert <ben@mctzock.de>
  * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
  */
+
+export function highlight(
+    element,
+    /** @type { {name: string, match: RegExp | RegExp[], style?: { color?: string, fontFamily?: string } }[] } */
+    patterns
+) {
+    if (element.tagName === "CODE") {
+
+        let origHTML = element.innerHTML;
+        let finalHTML = "";
+
+        patterns.forEach(pattern => {
+            const style = document.createElement('style')
+            style.type = "text/css";
+            style.innerHTML = `.${pattern.name} { ${pattern.style ? Object.entries(pattern.style).map(entry => entry.join(':')).join(';') : ''} }`;
+            document.getElementsByTagName("head")[0].appendChild(style);
+
+            let match = origHTML.match(pattern.match);
+
+            while (match) {
+                match = origHTML.match(pattern.match);
+                if (match) {
+                    let matchHTML = match[0];
+                    let matchIndex = origHTML.indexOf(matchHTML);
+                    let matchLength = matchHTML.length;
+                    let beforeHTML = origHTML.substring(0, matchIndex);
+                    let afterHTML = origHTML.substring(matchIndex + matchLength);
+                    finalHTML += beforeHTML;
+                    finalHTML += `<span class="${pattern.name}">${matchHTML}</span>`;
+                    origHTML = afterHTML;
+                }
+            }
+        });
+
+        element.innerHTML = finalHTML;
+
+        return true;
+    } else {
+        return false;
+    }
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,13 @@
+/**
+ * @author Ben Siebert <ben@mctzock.de>
+ * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
+ */
+export function highlight(element: any, patterns: {
+    name: string;
+    match: RegExp | RegExp[];
+    style?: {
+        color?: string;
+        fontFamily?: string;
+    };
+}[]): boolean;
 //# sourceMappingURL=index.d.ts.map
